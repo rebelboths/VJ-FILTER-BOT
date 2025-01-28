3ae35c1775bece24d7e7b4d0a0fc13fa3aedd6f7
rebelboths <rebelstar788jsjsj@gmail.com>
2025-01-28 19:05:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "7586947373:AAFawE0vup6_EIToNOaCa0CE_Ye-aNz
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
+PICS = (environ.get('PICS', 'https://envs.sh/aAF.jpg')).split()
 
 
 # Admins & Users
