bda9d12fdfd8abf55a6d9f1d1c1cc21308b0b50c
rebelboths <rebelstar788jsjsj@gmail.com>
2025-01-28 21:05:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ PICS = (environ.get('PICS', 'https://envs.sh/aAF.jpg')).split()
 
 
 # Admins & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6717382350 1009704073').split()] # For Multiple Id Use One Space Between Each.
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6717382350 1009704073 2116648189').split()] # For Multiple Id Use One Space Between Each.
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]  # For Multiple Id Use One Space Between Each.
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
